31d884bfea79d8b0dfbcec05e708b530310351ca
him_professor <him.hey.professor@gmail.com>
2025-06-16 16:11:41 +0700

    lifecyle hook and axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
-  <Navigation/>
+  <Navigation />
+  <p v-for="user in users" :key="user.id">
+    {{user.name}}
+  </p>
 </template>
 <script setup>
-import Navigation from '@/components/Navigation.vue';
+import Navigation from "@/components/Navigation.vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const users = ref([]);
+
+onMounted(async () => {
+  try {
+    await axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        users.value = res.data;
+      });
+  } catch (e) {
+    console.log("Axios fetching data error...");
+  }
+});
 </script>
